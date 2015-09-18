e76b1ebfa01f876c864c905f859ca057800481f3
orlygb25 <orly.gabay25@gmail.com>
2015-09-18 13:19:26 +0300

    Initialize

---
+++ AOACollections/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AOACollections.MvcApplication" Language="C#" %>
---
+++ AOACollections/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AOACollections.Startup))]
+namespace AOACollections
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
