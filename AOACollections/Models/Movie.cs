292c21554759e43f97b24d01377cc4378c8ca9f3
orlygb25 <orly.gabay25@gmail.com>
2015-09-19 19:06:32 +0300

    Add movie Fix style

---
+++ AOACollections/Models/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AOACollections.Models
+{
+    public class Movie
+    {
+            public int ID { get; set; }
+
+            [DisplayName("Movie Name")]
+            public string MovieName { get; set; }
+
+            [DisplayName("Description")]
+            public string Description { get; set; }
+
+            [DisplayName("Category")]
+            public Category Category { get; set; }
+
+            public Movie()
+            {
+                
+            }
+
+    }
+        public class MovieDBContext : DbContext
+        {
+            public DbSet<Movie> movie;
+            public System.Data.Entity.DbSet<AOACollections.Models.Movie> Movies { get; set; }
+        }
+
+        public enum Category
+        {
+            Drama,
+            Comedy,
+            Action,
+            Crime,
+            Fantasy
+        }
+}
