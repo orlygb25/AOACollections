ae48672e6a44937d00c48ad03e9c6e218725892d
orlygb25 <orly.gabay25@gmail.com>
2015-09-20 23:22:54 +0300

    Add customers

---
+++ AOACollections/Models/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AOACollections.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+            [DisplayName("User Name")]
+            public string UserName { get; set; }
+
+            [DisplayName("First Name")]
+            public string FirstName { get; set; }
+
+            [DisplayName("Last Name")]
+            public string LastName { get; set; }
+
+            [DisplayName("City")]
+            public string City { get; set; }
+
+            [DisplayName("Address")]
+            public string Address { get; set; }
+
+            [DisplayName("Gender")]
+            public Gender Gender { get; set; }
+
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            public DateTime RegistrationDate { get; set; }
+
+            public Customer()
+            {
+                RegistrationDate = DateTime.Now;
+            }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class CustomerDBContext : DbContext
+    {
+        public DbSet<Customer> order;
+        public System.Data.Entity.DbSet<AOACollections.Models.Customer> Orders { get; set; }
+    }
+}
